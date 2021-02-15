41cc3a2117f42da3f9d82c4799fd758ebce651e3
Vít Bednář <vit.bednar@assecosol.com>
2021-02-15 08:09:54 +0100

    VBE 2100215.0809

--- TaskEditor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2cb8961a-e6e2-457e-854d-578abdb9bc54")]
